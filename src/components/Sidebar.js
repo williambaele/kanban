4e8812fcf0f45f0022ed551f34bc7f420114a24b
William Baele <baelewilliam1999@gmail.com>
2023-10-12 10:30:12 +0200

    added router & fonts

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="bg-[#2C2C38] h-screen w-56 py-10 px-6 text-white  justify-around">
+    <div className="bg-[#2C2C38] h-screen w-56 py-10 text-white  justify-around">
       <div className="h-full flex flex-col">
-        <h2 className="text-2xl font-bold">KANBAN</h2>
-        <div className="w-full  grow py-10 space-y-4 flex flex-col">
-          <div className="w-full p-2 bg-gray-200 rounded-xl">
-            <p>Lorem ipsum</p>
+        <h2 className="text-2xl font-bold px-6">KANBAN</h2>
+        <div className="w-full  grow py-10 space-y-4 flex flex-col pr-2">
+          <div className="w-full p-2 bg-[#645FC6] rounded-r-xl cursor-pointer">
+            <p className="pl-6">Lorem ipsum</p>
           </div>
         </div>
         <div className="w-full h-10 bg-red-200">
