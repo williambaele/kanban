7d96b83761d2b42df1cdd0647291d670aa043d36
William Baele <baelewilliam1999@gmail.com>
2023-10-12 09:56:06 +0200

    added router & fonts

---
+++ src/components/Sidebar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="bg-[#2C2C38] h-screen w-56 py-10 px-6 text-white">
+      <h2 className="text-2xl font-bold">KANBAN</h2>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+
+const Home = () => {
+  return (
+    <div className="bg-[#21222D] h-screen">
+      <Sidebar />
+    </div>
+  );
+};
+
+export default Home;
