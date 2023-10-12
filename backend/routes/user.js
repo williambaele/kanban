1191ff84be0f768da029a457085e20acb8fa0215
William Baele <baelewilliam1999@gmail.com>
2023-10-12 13:45:20 +0200

    Added backend files

---
+++ backend/routes/user.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+// controller functions
+const {
+  loginUser,
+  signupUser,
+  getUser,
+  updateUser,
+  getUsers,
+} = require("../controllers/userController");
+
+const router = express.Router();
+
+// login route
+router.post("/login", loginUser);
+
+//GET a single user
+router.get("/:id", getUser);
+
+//GET ALL USERS
+router.get("/", getUsers);
+
+//UPDATE USER
+router.patch("/:id", updateUser);
+
+// signup route
+router.post("/signup", signupUser);
+
+module.exports = router;
